dd3499dd0e88ee195b23321e9c87635089627e6e
Raúl Doezón <santor3medio@gmail.com>
2018-10-24 15:55:06 -0500

    #81. Remove the functionality to open dropdown in the navbar. Instead, the hover effect is used. Delete in menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -84,22 +84,3 @@ hideHamburger.addEventListener('click', function(e) {
   e.preventDefault();
   hideMenu();
 });
-
-function dropDown() {
-    document.getElementById("navDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdown-btn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
